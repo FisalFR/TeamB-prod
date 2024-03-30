7e4d78023b744aa9df35843e491bdc1fe1911bfe
cnguyen111 <cnguyen1@wpi.edu>
2024-03-30 13:34:08 -0400

    Transferred my functions from proto to prod

---
+++ apps/backend/src/writeEdge.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+import { PrismaClient } from "../../../packages/database/.prisma/client";
+const prisma = new PrismaClient();
+
+class writeEdge {
+  static async edgesBackToCSV() {
+    //fetch the data from the table
+    const edgesData = await prisma.l1Edges.findMany();
+
+    //creates the header row in the csv file
+    const headersEdges = ["startNodeID", "endNodeID"];
+
+    // create the rows for the csv file
+    let finalEdgesString = headersEdges.join(",") + "\n";
+    edgesData.forEach((edge) => {
+      const row = [edge.startNodeID, edge.endNodeID];
+      finalEdgesString += row.join(",") + "\n";
+    });
+    // use fs to write to the csv file
+    fs.writeFileSync("../../data/edgesBack.csv", finalEdgesString);
+    console.log("Wrote to the data edgesBack.csv file");
+  }
+}
+export default writeEdge;
