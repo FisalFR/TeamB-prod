a03d280bef57843acba19d0061f986365f5c66db
tacheampong <th.acheampong@gmail.com>
2024-04-01 18:36:38 -0400

    added styling using a card format to the log page only for interpreter

--- apps/frontend/src/routes/requests-log-page.tsx
+++ apps/frontend/src/routes/requests-log-page.tsx
@@ -4,8 +4,11 @@ import {LanguageInterpreterTypes} from 'packages/common/src/languageInterpreterT
 import axios from "axios";
 //import axios from "axios";
 export function RequestLogs(){
+
+
     //const [request, setRequest] = useState<LanguageInterpreterType>({language: "", location: ""});
     const [request,setRequest] = useState<LanguageInterpreterTypes[]>([]);
+
         useEffect(()=>{
 
             axios.get("/api/languageInterpreter").then((response)=>{
@@ -15,19 +18,31 @@ export function RequestLogs(){
                 }));
 
                 console.log("Response data", response.data);
+
                 setRequest(requestData);
             });
 
 
         },[]);
+
+
     return (
-        <div className="flex flex-col">
+
+        <div className="flex flex-col gap-10 w-96">
+
             {request.length > 0 ? (
                 request.map((request, index) => (
-                    <div key={index}>
-                        <p>Language: {request.language}</p>
-                        <p>Location: {request.location}</p>
+                    <div>
+                        <h2 className="font-HeadlandOne text-2xl pb-3">Language Reguest Log {index + 1}</h2>
+
+                <div className="border-2 border-black p-2 rounded-2xl " key={index}>
+
+                        <p className="font-OpenSans pb-5">Language: {request.language}</p>
+                        <p className="font-OpenSans pb-5">Location: {request.location}</p>
+
                     </div>
+                    </div>
+
                 ))
             ) : (
                 <div>No requests</div>
