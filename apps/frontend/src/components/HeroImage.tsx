27120b05360887510ef4382e844ad1515963be1e
jdlogan0 <143477762+jdlogan0@users.noreply.github.com>
2024-04-02 18:58:15 -0400

    fixing styling for hero image

--- apps/frontend/src/components/HeroImage.tsx
+++ apps/frontend/src/components/HeroImage.tsx
@@ -3,7 +3,7 @@ import MazeLineComponent from "../components/mazeLineComponent.tsx";
 
 export default function HeroImage() {
     return (
-        <div className="aspect-[1/1] max-w-lg max-h-lg bg-white rounded-full p-3 drop-shadow-md overflow-hidden">
+        <div className="w-3/5 max-w-lg max-h-lg bg-white rounded-full p-3 drop-shadow-md overflow-hidden">
             <MazeLineComponent />
         </div>
     );
