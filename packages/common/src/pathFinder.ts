cd8525ff1fd9697a02286c5b10691728126b1f82
mominhnguyen <61259039+mominhnguyen@users.noreply.github.com>
2024-04-01 20:43:49 -0400

    Added untested A*

--- packages/common/src/pathFinder.ts
+++ packages/common/src/pathFinder.ts
@@ -1,6 +1,7 @@
 import Node from "./node";
 import Edge from "./edge";
 import Queue from "./Queue";
+import PriorityQueue from "./PriorityQueue.ts";
 
 class Path {
   nodeList: Node[];
@@ -14,6 +15,39 @@ class Path {
     this.finalPath = [];
     this.nodeMap = new Map<string, Node>();
   }
+
+  // Heuristics
+    heuristic(a: Node, b: Node): number {
+      return Math.sqrt((a.xcoord - b.xcoord) ** 2 + (a.ycoord - b.ycoord) ** 2);
+    }
+
+
+  // Function to return the path from startNode to endNode using A*
+    AStar(startNode: Node, endNode: Node): Node[] {
+        const frontier: PriorityQueue<Node> = new PriorityQueue<Node>();
+        frontier.insert(startNode, 0);
+        const cameFrom = new Map();
+        const costSoFar: Map<Node, number> = new Map();
+        cameFrom.set(startNode, null);
+        costSoFar.set(startNode, 0);
+
+        while (!frontier.isEmpty()) {
+        const current = frontier.pop()!;
+        if (current.nodeID == endNode.nodeID) {
+            break;
+        }
+        for (const next of current.neighbors) {
+            const newCost = costSoFar.get(current)! + 1;
+            if (!costSoFar.has(next) || newCost < costSoFar.get(next)!) {
+            costSoFar.set(next, newCost);
+            const priority = newCost + this.heuristic(endNode, next);
+            frontier.insert(next, priority);
+            cameFrom.set(next, current);
+            }
+        }
+        }
+        return this.reconstructPath(cameFrom, startNode, endNode);
+    }
 
   // Function to return the path from startNode to endNode using BFS
   BFS(startNode: Node, endNode: Node): Node[] {
