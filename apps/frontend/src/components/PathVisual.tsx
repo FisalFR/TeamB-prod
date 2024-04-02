8a56082b69fd30c07c09e587333f88020e2d19be
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-04-02 17:42:59 -0400

    Completed map pathing animation

--- apps/frontend/src/components/PathVisual.tsx
+++ apps/frontend/src/components/PathVisual.tsx
@@ -1,77 +1,3 @@
-// import { JSX } from "react/jsx-runtime";
-// import { motion } from "framer-motion";
-//
-// function PathVisual(props: { path: number[][]; image: string; width: number; height: number; scale:number; showPath: boolean} ) {
-//     const pathArray = props.path;
-//
-//     const draw = {
-//         hidden: { pathLength: 0, opacity: 1 },
-//         visible: {
-//             opacity: 1,
-//             pathLength: 1,
-//             transition: {
-//                 duration: 5,
-//                 ease: "linear",
-//             },
-//         }
-//     };
-//
-//     function createPaths() {
-//         const lines: JSX.Element[] = [];
-//         for (let i = 0; i < pathArray.length - 1; i++){
-//             lines.push(
-//                 <line
-//                     x1={pathArray[i][0]}
-//                     y1={pathArray[i][1]}
-//                     x2={pathArray[i+1][0]}
-//                     y2={pathArray[i+1][1]}
-//                     stroke="#009CA6"
-//                     strokeWidth={4}
-//                 />);
-//         }
-//         return lines;
-//     }
-//
-//     function LinesToPath({ lines }) {
-//         const pathD = lines.reduce((acc, line) => {
-//             const { props } = line;
-//             const { x1, y1, x2, y2 } = props;
-//             return acc + `M ${x1} ${y1} L ${x2} ${y2} `;
-//         }, '');
-//
-//         // Close the path by connecting the last point to the first point
-//         const fullPathD = pathD + 'Z';
-//
-//         return (
-//             <motion.path d={fullPathD} stroke="#009CA6" fill="none" variants={draw} initial="hidden" animate="visible"/>
-//         );
-//     }
-//
-//
-//     const viewBox = "0 0 " + (props.width)  + " " + (props.height);
-//
-//     return (
-//         <motion.svg width={props.width * props.scale}
-//                     height={props.height * props.scale}
-//                     viewBox={viewBox}
-//                     initial="hidden"
-//                     animate="visible"
-//                     variants={draw}>
-//             <image xlinkHref={props.image} width={props.width} height={props.height}></image>
-//             <LinesToPath lines={createPaths()} />
-//             function
-//             <circle cx={pathArray[0][0]} cy={pathArray[0][1]} r={8} fill="blue"/>
-//             <circle cx={pathArray[pathArray.length - 1][0]} cy={pathArray[pathArray.length - 1][1]} r={8} fill="blue"/>
-//         </motion.svg>
-//     );
-//
-// }
-//
-// export default PathVisual;
-
-
-
-// import { JSX } from "react/jsx-runtime";
 import { motion } from "framer-motion";
 
 function PathVisual(props: { path: number[][]; image: string; width: number; height: number; scale:number; showPath: boolean} ) {
@@ -90,12 +16,13 @@ function PathVisual(props: { path: number[][]; image: string; width: number; hei
     };
 
     function createPath() {
-        let path = `M ${pathArray[0][0]} ${pathArray[0][1]}`;
-        for (let i = 1; i < pathArray.length; i++) {
-            path += ` L ${pathArray[i][0]} ${pathArray[i][1]}`;
+        if(props.showPath){
+            let path = `M ${pathArray[0][0]} ${pathArray[0][1]}`;
+            for (let i = 1; i < pathArray.length; i++) {
+                path += ` L ${pathArray[i][0]} ${pathArray[i][1]}`;
+            }
+            return path;
         }
-        console.log(path);
-        return path;
     }
 
     const viewBox = "0 0 " + (props.width)  + " " + (props.height);
@@ -108,12 +35,12 @@ function PathVisual(props: { path: number[][]; image: string; width: number; hei
                     animate="visible"
                     variants={draw}>
             <image xlinkHref={props.image} width={props.width} height={props.height}></image>
-            <motion.path d={createPath()} stroke="#009CA6" fill="none" initial="hidden"
+            <motion.path d={createPath()} stroke="#009CA6" fill="none" initial="hidden" stroke-width={4}
                          animate="visible"
                          variants={draw}/>
-            <circle cx={pathArray[0][0]} cy={pathArray[0][1]} r={8} fill="blue"/>
             <circle cx={pathArray[pathArray.length - 1][0]} cy={pathArray[pathArray.length - 1][1]} r={8}
-                    fill="blue"/>
+                    fill="#F6BD38"/>
+            <circle cx={pathArray[0][0]} cy={pathArray[0][1]} r={8} fill="#012D5A"/>
         </motion.svg>
     );
 }
--- apps/frontend/src/routes/map.tsx
+++ apps/frontend/src/routes/map.tsx
@@ -8,22 +8,23 @@ import {startEndNodes} from "common/src/pathfinding.ts";
 
 export function Map(){
 
-    //const testPathPoints = [
-    //    [2665, 1043], [2490, 1043], [2445, 1043], [2310, 1043], [2215, 1045], [2220, 974], [2220, 904], [2185, 904], [2130, 904]];
-
     const [zoom, setZoom] = useState(0.2);
+    const [pathPoints, setPathPoints] = useState([0, 0]);
+    const [showPath, setShowPath] = useState(false);
+
     const [request, setRequest] = useState<startEndNodes>({startNode: "", endNode: ""});
 
     const [nodes, setNodes] = useState(["Error accessing map points"]);
 
-    const [nodeStringToID, setNodesStringToID] = useState({});
+    const [nodeData, setNodeData] = useState({});
 
-    const [pathPoints, setPathPoints] = useState([2665, 1043]);
+
 
     const divRef = useRef<HTMLDivElement>(null);
     function zoomSlider(e: ChangeEvent<HTMLInputElement>) {
         setZoom(0.2 * parseFloat(e.target.value));
-        (divRef.current as HTMLDivElement).scroll(1000 - 1000/parseFloat(e.target.value), parseFloat(e.target.value));
+        //const scrollDiv = (divRef.current as HTMLDivElement);
+        //scrollDiv.scroll(scrollDiv.scrollLeft + (scrollDiv.scrollWidth-scrollDiv.clientWidth)/2, 0);
     }
 
     function findPath() {
@@ -38,6 +39,7 @@ export function Map(){
                     nodeCoords.push(response.data.nodes[i]);
                 }
                 setPathPoints(nodeCoords);
+                setShowPath(true);
             }
             else {
                 alert("No path on Floor Lower Level 1 with selected nodes");
@@ -46,40 +48,49 @@ export function Map(){
     }
 
     function handleStartChange(e: ChangeEvent<HTMLInputElement>) {
-        setRequest({...request, startNode: nodeStringToID[e.target.value]});
+        setRequest({...request, startNode: nodeData[e.target.value].id});
+        setPathPoints([nodeData[e.target.value].coords, pathPoints[pathPoints.length-1]]);
+        setShowPath(false);
     }
     function handleEndChange(e: ChangeEvent<HTMLInputElement>) {
-        setRequest({...request, endNode: nodeStringToID[e.target.value]});
+        setRequest({...request, endNode: nodeData[e.target.value].id});
+        setPathPoints([pathPoints[0], nodeData[e.target.value].coords]);
+        setShowPath(false);
     }
 
     useEffect( () => {
         axios.get("/api/pathfinding/").then((response) => {
             const nodeStrings = [];
-            const strToID = {};
+            const tempNodeData = {};
             for (let i = 0; i < response.data.length; i++) {
                 nodeStrings.push(response.data[i].longName);
-                strToID[response.data[i].longName] = response.data[i].nodeID;
+                tempNodeData[response.data[i].longName] = {id: response.data[i].nodeID, coords: [response.data[i].xcoord, response.data[i].ycoord]};
             }
             setNodes(nodeStrings);
-            setNodesStringToID(strToID);
-            setRequest({startNode: strToID[nodeStrings[0]], endNode: strToID[nodeStrings[0]]});
+            setNodeData(tempNodeData);
+            setRequest({startNode: tempNodeData[nodeStrings[0]].id, endNode: tempNodeData[nodeStrings[0]].id});
+            setPathPoints([tempNodeData[nodeStrings[0]].coords]);
         });
     }, []);
 
     return (
-        <div div className="centerContent gap-10 w-full h-fit">
-            <div>
+        <div className="centerContent gap-10 w-full h-full">
+            <div className="">
                 <p className="font-HeadlandOne text-3xl py-3">Floor: Lower Level 1</p>
-                <div className="w-fit h-fit max-w-[1000px] max-h-[690px] overflow-scroll" ref={divRef}>
-                    <PathVisual key={JSON.stringify(pathPoints)} path={pathPoints} image={ll1map} width={5000} height={3400} scale={zoom}/>
+                <div className="w-fit h-fit max-w-[1000px] max-h-[calc(100vh-200px)] overflow-scroll" ref={divRef}>
+                    <PathVisual key={JSON.stringify(pathPoints)} path={pathPoints} image={ll1map} width={5000} height={3400}
+                            scale={zoom} showPath={showPath}/>
                 </div>
-                <br/>
-                <input type="range" min="1" max="10" step="any" defaultValue="1" onChange={zoomSlider} id="myRange"></input>
                 <br/><br/>
             </div>
-            <div>
-                <Select label="Starting node: " id="nodeStartSelect" options={nodes} onChange={handleStartChange}/><br/><br/>
-                <Select label="Ending node: " id="nodeEndSelect" options={nodes} onChange={handleEndChange}/><br/><br/>
+            <div className="text-left w-fit">
+                <span className="text-3xl">- </span>
+                <input type="range" min="1" max="10" step="any" defaultValue="1" onChange={zoomSlider}
+                       id="myRange"></input><span className="text-3xl"> +</span><br/><br/>
+                <span style={{color: "#012D5A"}}>● </span>
+                <Select label="Starting Location: " id="nodeStartSelect" options={nodes} onChange={handleStartChange}/><br/><br/>
+                <span style={{color: "#F6BD38"}}>● </span>
+                <Select label="Ending Location: " id="nodeEndSelect" options={nodes} onChange={handleEndChange}/><br/><br/>
                 <Button onClick={findPath} children={"Find Path"}/>
             </div>
         </div>
