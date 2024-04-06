9c77b463525f24bed0da6a1f589f1d209c01dfb6
mominhnguyen <61259039+mominhnguyen@users.noreply.github.com>
2024-04-06 16:04:15 -0400

    log book in progress

--- apps/frontend/src/routes/Database.tsx
+++ apps/frontend/src/routes/Database.tsx
@@ -3,9 +3,9 @@
 // import Table from "../components/Table.tsx";
 
 import Dropdown from "../components/dropdown.tsx";
-import {useState} from "react";
+import Table from "../components/Table.tsx";
+import React, {useState} from "react";
 import {MaintenanceRequest} from "common/src/maintenanceRequest.ts";
-
 function LogBook() {
 
     // const [interpreterReq, setInterpreterReq] = useState([]);
@@ -34,31 +34,71 @@ function LogBook() {
 
     return (
         <div className="">
-            <h2 className={"font-extrabold text-3xl font-HeadlandOne"}>Filter Data</h2>
+            {/*Form to filter current requests*/}
+            <div>
+                <form
+                    className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-3 gap-4">
+                    <h2 className={"font-extrabold text-3xl font-HeadlandOne flex items-start"}>Filter Data</h2>
+                    <p className={"text-left font-bold"}>Request Type</p>
+                    <Dropdown options={requestTypeOptions} placeholder={"Choose Request Type"}
+                              name={"requestTypeDropdown"}
+                              id={"dropdown1"} value={cleared}
+                              setInput={handleRequestType} required={true}/>
+                </form>
 
-            <form
-                className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-10 gap-4">
-                <p className={"text-left font-bold"}>Request Type</p>
-                <Dropdown options={requestTypeOptions} placeholder={"Choose Request Type"} name={"requestTypeDropdown"}
-                          id={"dropdown1"} value={cleared}
-                          setInput={handleRequestType} required={true}/>
-            </form>
+                <form
+                    className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-3 gap-4">
+                    <p className={"text-left font-bold"}>Request Status</p>
+                    <Dropdown options={requestTypeOptions} placeholder={"Choose Status"} name={"requestTypeDropdown"}
+                              id={"dropdown1"} value={cleared}
+                              setInput={handleRequestType} required={true}/>
+                </form>
 
-            <form
-                className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-10 gap-4">
-                <p className={"text-left font-bold"}>Request Status</p>
-                <Dropdown options={requestTypeOptions} placeholder={"Choose Status"} name={"requestTypeDropdown"}
-                          id={"dropdown1"} value={cleared}
-                          setInput={handleRequestType} required={true}/>
-            </form>
+                <form
+                    className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-3 gap-4">
+                    <p className={"text-left font-bold"}>Assigned Staff</p>
+                    <Dropdown options={requestTypeOptions} placeholder={"Assigned Staff"} name={"requestTypeDropdown"}
+                              id={"dropdown1"} value={cleared}
+                              setInput={handleRequestType} required={true}/>
+                </form>
+            </div>
 
-            <form
-                className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-10 gap-4">
-                <p className={"text-left font-bold"}>Assigned Staff</p>
-                <Dropdown options={requestTypeOptions} placeholder={"Assigned Staff"} name={"requestTypeDropdown"}
-                          id={"dropdown1"} value={cleared}
-                          setInput={handleRequestType} required={true}/>
-            </form>
+
+            {/*Form to assign requests*/}
+            <div>
+                <form
+                    className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-3 gap-4">
+                    <h2 className={"font-extrabold text-3xl font-HeadlandOne flex items-start"}>Assign Staff
+                        Request</h2>
+                    <p className={"text-left font-bold"}>Request Type</p>
+                    <Dropdown options={requestTypeOptions} placeholder={"Choose Request Type"}
+                              name={"requestTypeDropdown"}
+                              id={"dropdown1"} value={cleared}
+                              setInput={handleRequestType} required={true}/>
+                </form>
+
+                <form
+                    className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-3 gap-4">
+                    <p className={"text-left font-bold"}>Request Status</p>
+                    <Dropdown options={requestTypeOptions} placeholder={"Choose Status"} name={"requestTypeDropdown"}
+                              id={"dropdown1"} value={cleared}
+                              setInput={handleRequestType} required={true}/>
+                </form>
+
+                <form
+                    className="border-solid border-deep-blue border-r-2 w-[30vw] h-full flex flex-col items-start p-3 gap-4">
+                    <p className={"text-left font-bold"}>Assigned Staff</p>
+                    <Dropdown options={requestTypeOptions} placeholder={"Assigned Staff"} name={"requestTypeDropdown"}
+                              id={"dropdown1"} value={cleared}
+                              setInput={handleRequestType} required={true}/>
+                </form>
+            </div>
+
+            {/*Actual Database Table starts here*/}
+            <div className="max-h-[60vh] overflow-scroll border-solid border-b-[1px] border-deep-blue">
+                <Table data={maintenanceReq} headings={["Issue", "Location", "Urgent", "Feedback"]}
+                       keys={["issue", "location", "isUrgent", "feedback"]}/>
+            </div>
 
         </div>
     );
