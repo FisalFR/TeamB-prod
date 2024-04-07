1ce640feaa0071c927718f7e80a79fe22d41213e
cnguyen111 <cnguyen1@wpi.edu>
2024-04-07 14:17:28 -0400

    deleted repeated files

--- packages/common/src/MedicineRequestType 3.ts
+++
@@ -1,8 +0,0 @@
-export type MedicineRequestType = {
-    employeeName: string,
-    priority: string,
-    location: string,
-    medicine: string,
-    quantity: 0,
-    additionalComments: string
-}
