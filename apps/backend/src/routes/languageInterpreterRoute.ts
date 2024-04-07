78587b49616c05174c65314aaf3365bfe49787f6
cnguyen111 <cnguyen1@wpi.edu>
2024-04-07 13:10:12 -0400

    connected locations to database

--- apps/backend/src/routes/languageInterpreterRoute.ts
+++ apps/backend/src/routes/languageInterpreterRoute.ts
@@ -15,15 +15,15 @@ router.get("/", async (req, res) => {
   res.status(200).json(formsWithLanguageStrings);
 });
 
-router.get("/:index", (req, res) => {
-  const index = parseInt(req.params.index);
-  if (index >= 0 && index < database.length) {
-    res.status(200).json(database[index]);
-  } else {
-    res.status(400).json({
-      message: "not a valid index",
-    });
-  }
+router.get("/location", async (req, res) => {
+  const nodeType = await client.nodes.findMany({
+    where: {
+      NOT: {
+        longName: { search: "Hall" },
+      },
+    },
+  });
+  res.status(200).json(nodeType);
 });
 
 router.post("/search", async (req, res) => {
