4930d81f949dd34faa28695df0745fcbffa16e15
Jeremy Mulia Kurtz <kurtzjeremy126@gmail.com>
2024-04-09 10:49:01 -0400

    changed highlight to gold yellow

--- apps/frontend/src/components/map/MapFloorButton.tsx
+++ apps/frontend/src/components/map/MapFloorButton.tsx
@@ -3,7 +3,7 @@ import React, {ReactNode} from "react";
 
 function MapFloorButton(props:{onClick:(e: React.MouseEvent) => void, highlighted: boolean ,children: ReactNode}) {
     const buttonClass = props.highlighted ?
-        "p-4 px-5 bg-deep-blue font-bold text-white w-full rounded ring-4 ring-bwh-cyan" :
+        "p-4 px-5 bg-deep-blue font-bold text-white w-full rounded ring-4 ring-gold-yellow" :
         "p-4 px-5 bg-deep-blue font-bold text-white w-full rounded";
 
     return(
