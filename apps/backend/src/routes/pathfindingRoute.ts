cea3db04d0190e3a799db2d9dc9ecec1565d81fd
mominhnguyen <61259039+mominhnguyen@users.noreply.github.com>
2024-04-09 22:00:35 -0400

    Added back poor import

--- apps/backend/src/routes/pathfindingRoute.ts
+++ apps/backend/src/routes/pathfindingRoute.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { startEndNodes } from "common/src/pathfinding";
+import { startEndNodes } from "../../../../packages/common/src/pathfinding";
 import Path from "../../../../packages/common/src/pathFinder";
 //import Parser from "../../../../packages/common/src/parser";
 import Node from "../../../../packages/common/src/node";
