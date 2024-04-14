5a30c4f3f631d34f765b707d4e486cd6f9e3c65a
tacheampong <th.acheampong@gmail.com>
2024-04-14 00:51:28 -0400

    added Auth0 to navigation bar

--- apps/frontend/src/components/NavigationBar.tsx
+++ apps/frontend/src/components/NavigationBar.tsx
@@ -2,8 +2,20 @@ import {NavLink} from "./NavLink.tsx";
 import bwhLogo from "../assets/bwh-logo-white.svg";
 import NavDropDown from "../components/navDropDown.tsx";
 import user_icon from "../assets/user_icon.svg";
+import {useAuth0} from "@auth0/auth0-react";
+import LoginNavigationBar from "./LoginNavigationBar.tsx";
+import HandleLogout from "./handleLogout.tsx";
+
+
+
 export function NavigationBar() {
+    const {isAuthenticated, isLoading} = useAuth0();
+
+    console.log(isAuthenticated);
+    console.log(isLoading);
+
     {
+
         const dropdownList = [
             ["/maintenance", "Maintenance"],
             ["/interpreter", "Interpreter"],
@@ -19,29 +31,37 @@ export function NavigationBar() {
         ];
 
         return (
+            <div>
+                {(isAuthenticated && !isLoading) ? (
+                    <>
+                        <div className="navbar z-50 bg-deep-blue static h-14 top-0 left-0 grid w-full">
+                            <img className="h-3/6 self-center px-4" src={bwhLogo}
+                                 alt="Brighams Logo White"></img>
+                            <nav className="uppercase divide-x divide-solid centerContent w-fit justify-self-center">
 
-            <>
-            <div className="navbar z-50 bg-deep-blue static h-14 top-0 left-0 grid w-full">
-                <img className="h-3/6 self-center px-4" src={bwhLogo}
-                     alt="Brighams Logo White"></img>
-                <nav className="uppercase divide-x divide-solid centerContent w-fit justify-self-center">
+                                <NavDropDown mainLink= {["", "Request"]} dropdownLinks={dropdownList}/>
 
-                    <NavDropDown mainLink= {["", "Request"]} dropdownLinks={dropdownList}/>
+                                <div className="px-16">
+                                    <NavLink href="/map">Map</NavLink>
+                                </div>
 
-                    <div className="px-16">
-                        <NavLink href="/map">Map</NavLink>
-                    </div>
+                                <NavDropDown mainLink={["", "Admin"]} dropdownLinks={adminDropdownList}></NavDropDown>
+                            </nav>
+                            <div className="self-center">
+                                <div className="float-end centerContent px-16">
 
-                    <NavDropDown mainLink={["", "Admin"]} dropdownLinks={adminDropdownList}></NavDropDown>
-                </nav>
-                <div className="self-center">
-                    <div className="float-end centerContent px-16">
-                        <NavLink href="/"><div className="flex flex-row gap-2">
-                            <img src={user_icon} alt="Username icon" height="20" width="20"/>LOGOUT</div></NavLink>
-                    </div>
-                </div>
+                                    <NavLink onClick={HandleLogout}><div className="flex flex-row gap-2">
+                                        <img src={user_icon} alt="Username icon" height="20" width="20"/>LOGOUT</div></NavLink>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                ): (
+                    <LoginNavigationBar/>
+                )}
             </div>
-            </>
+
+
         );
     }
 }
