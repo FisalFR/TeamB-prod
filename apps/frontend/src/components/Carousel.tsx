eea59f7fdc7abe66c14e18cc36c4a32efc1b60f1
tacheampong <th.acheampong@gmail.com>
2024-04-16 16:54:14 -0400

    added a little bit of padding to the text

--- apps/frontend/src/components/Carousel.tsx
+++ apps/frontend/src/components/Carousel.tsx
@@ -14,7 +14,7 @@ const Carousel = ({ content, autoPlay, interval }) => {
     }, [autoPlay, interval, content.length]);
 
     return (
-        <div  className=" relative w-full select-none">
+        <div  className="pr-2 relative w-full select-none">
             {content.map((item, index) => (
                 <div onClick={HandleLogin({path:item.text.route})} key={index} className={`flex items-center space-x-4 ${currentIndex === index ? 'flex' : 'hidden'}`} style={{ height: '100%' }}>
                     <img src={item.image.src} alt={item.image.alt} className="w-1/2 h-1/2 object-cover" />
