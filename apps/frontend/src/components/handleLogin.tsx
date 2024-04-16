b348163d1bd4194e63bf5531e8094718e0375c4c
tacheampong <th.acheampong@gmail.com>
2024-04-16 16:28:16 -0400

    added prop to handle login - takes a path

--- apps/frontend/src/components/handleLogin.tsx
+++ apps/frontend/src/components/handleLogin.tsx
@@ -1,10 +1,9 @@
 import auth0 from "../contexts/auth0-client.ts";
 
 
-export function HandleLogin() {
+export function HandleLogin(props:{path:string}) {
     console.log("login");
-    const redirectUri = `${window.location.origin}/map`;
-
+    const redirectUri = `${window.location.origin.concat(props.path)}`;
     return async () => {
 
         await auth0.loginWithRedirect({
