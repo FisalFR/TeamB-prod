0c1b7a82b4e568d73fe2253a1b69c0ef618274e3
tacheampong <th.acheampong@gmail.com>
2024-04-16 16:30:58 -0400

    added authentication into carousel for each request

--- apps/frontend/src/components/Carousel.tsx
+++ apps/frontend/src/components/Carousel.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
+//import { Link } from 'react-router-dom';
+//import {NavLink} from "./NavLink.tsx";
+//import handleLogin from "./handleLogin.tsx";
+import {HandleLogin} from "./handleLogin.tsx";
 const Carousel = ({ content, autoPlay, interval }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -14,16 +16,21 @@ const Carousel = ({ content, autoPlay, interval }) => {
     }, [autoPlay, interval, content.length]);
 
     return (
-        <div className="relative w-full select-none">
+        <div  className=" relative w-full select-none">
             {content.map((item, index) => (
-                <div key={index} className={`flex items-center space-x-4 ${currentIndex === index ? 'flex' : 'hidden'}`} style={{ height: '100%' }}>
+                <div onClick={HandleLogin({path:item.text.route})} key={index} className={`flex items-center space-x-4 ${currentIndex === index ? 'flex' : 'hidden'}`} style={{ height: '100%' }}>
                     <img src={item.image.src} alt={item.image.alt} className="w-1/2 h-1/2 object-cover" />
                     <div className="w-1/2 h-1/2 p-lg">
                         <h4 className="text-xl font-bold">{item.text.title}</h4>
                         <p>{item.text.description}</p>
-                        <Link to={item.text.route} className="mt-4 text-blue-500 bg-origin-padding">
-                            {item.text.callToAction}
-                        </Link>
+
+                        <div className="">
+                            <p onClick={HandleLogin({path:item.text.route}) } className="mt-4 hover:cursor-pointer text-deep-blue" >
+                                {item.text.callToAction}
+                            </p>
+                        </div>
+
+
                     </div>
                 </div>
             ))}
