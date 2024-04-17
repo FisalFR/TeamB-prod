cd3727649ad349580d70f7815c5a014081f0b646
tacheampong <th.acheampong@gmail.com>
2024-04-17 18:56:46 -0400

    removed conditional redirection

--- apps/frontend/src/components/handleLogin.tsx
+++ apps/frontend/src/components/handleLogin.tsx
@@ -4,11 +4,6 @@ import auth0 from "../contexts/auth0-client.ts";
 export function HandleLogin(props:{path:string}) {
     console.log("login");
     const redirectUri = `${window.location.origin.concat(props.path)}`;
-
-    if(redirectUri === `${window.location.origin.concat('/map')}` ){
-        return window.location.href = `${window.location.origin.concat('/map')}`;
-
-    }
     const login = async () => {
         await auth0.loginWithRedirect({
             authorizationParams:{
