bf1030f7191b5420c6d4a841a4b3f0645903421d
mominhnguyen <61259039+mominhnguyen@users.noreply.github.com>
2024-04-16 20:28:21 -0400

    Fixed Merge Conflicts

--- apps/frontend/src/components/map/PathSelector.tsx
+++ apps/frontend/src/components/map/PathSelector.tsx
@@ -34,13 +34,23 @@ export function PathSelector(props: {
                 <div>
                     <div className="grid grid-cols-[auto_1fr] grid-rows-3 h-fit justify-items-center items-center pt-2 pr-2 pl-2">
                         <img src={fromIcon} alt="from" className="px-1"/>
-                        <Select label="" id="nodeStartSelect" options={props.options}
+                        <Select label="" id="nodeStartSelect" options={props.nodes.map((node) =>{
+                            return node.nodeID;
+                        })}
+                                display = {props.nodes.map((node) =>{
+                                    return node.longName;
+                                })}
                                 onChange={props.handleStartChange as (e: React.ChangeEvent<HTMLSelectElement>) => void}
                                 defaultOption="Select your start location"/>
                         <img src={dots} alt="dots" className="h-7 pb-1 px-1"/>
                         <div></div>
                         <img src={destination} alt="destination" className="px-1"/>
-                        <Select label="" id="nodeEndSelect" options={props.options}
+                        <Select label="" id="nodeEndSelect" options={props.nodes.map((node) =>{
+                            return node.nodeID;
+                        })}
+                                display = {props.nodes.map((node) =>{
+                                    return node.longName;
+                                })}
                                 onChange={props.handleEndChange as (e: React.ChangeEvent<HTMLSelectElement>) => void}
                                 defaultOption="Select your end location"/>
                     </div>
@@ -54,47 +64,7 @@ export function PathSelector(props: {
                             <AlgorithmButtons px="px-8" onClick={props.onClick3} isActive={props.selectedAlgo === "Dijkstra"}> DIJKSTRA </AlgorithmButtons>
                         </div>
                     </div>
-                </div>
-            )}
-    return <div
-        className="absolute top-5 left-5 flex flex-col bg-white h-fit rounded-xl items-end">
-        <div className="grid grid-cols-[auto_1fr] grid-rows-3 h-fit justify-items-center items-center pt-2 pr-2 pl-2">
-            <img src={from} alt="from" className={"px-1"}/>
-            <Select label="" id="nodeStartSelect"
-                    options={props.nodes.map((node) =>{
-                        return node.nodeID;
-                    })}
-                    display = {props.nodes.map((node) =>{
-                        return node.longName;
-                    })}
-                    onChange={props.handleStartChange as (e: React.ChangeEvent<HTMLSelectElement>) => void}
-                    defaultOption={ props.selectedStartOption !== undefined ? props.selectedStartOption : "Select your start location"}/>
-            <img src={dots} alt="dots" className={"h-7 pb-1 px-1"}/>
-            <div></div>
-            <img src={destination} alt="destination" className={"px-1"}/>
-            <Select label="" id="nodeEndSelect"
-                    options={props.nodes.map((node) =>{
-                        return node.nodeID;
-                    })}
-                    display = {props.nodes.map((node) =>{
-                        return node.longName;
-                    })}
-                    onChange={props.handleEndChange as (e: React.ChangeEvent<HTMLSelectElement>) => void}
-                    defaultOption={ props.selectedEndOption !== undefined ? props.selectedEndOption :  "Select your end location"}/>
-        </div>
-        <div
-            className="flex flex-row justify-center mt-2 w-full bg-deep-blue rounded-br-xl rounded-bl-xl font-OpenSans items-center font-bold text-bone-white">
-            <div className="divide-x divide-solid py-2 flex flex-row">
-                <AlgorithmButtons px="px-8" onClick={props.onClick} isActive={props.selectedAlgo === "Astar"}>
-                    A <img src={star} alt="A Star Icon" height="15" width="15"/>
-                </AlgorithmButtons>
-                <AlgorithmButtons px="px-8" onClick={props.onClick1}
-                                  isActive={props.selectedAlgo === "BFS"}> BFS </AlgorithmButtons>
-                <AlgorithmButtons px="px-8" onClick={props.onClick2}
-                                  isActive={props.selectedAlgo === "DFS"}> DFS </AlgorithmButtons>
-                <AlgorithmButtons px="px-8" onClick={props.onClick3}
-                                  isActive={props.selectedAlgo === "Dijkstra"}> DIJKSTRA </AlgorithmButtons>
+                </div>)}
             </div>
-        </div>
-    );
-}
+            );
+};
