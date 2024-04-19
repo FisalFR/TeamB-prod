d2480b558a96e70202ca5732697dca0b6410b47e
HenryHribar <hhribar21@gmail.com>
2024-04-18 22:09:18 -0400

    Completed redesign of path selector.

--- apps/frontend/src/components/map/PathSelector.tsx
+++ apps/frontend/src/components/map/PathSelector.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import fromIcon from "../../assets/from_to_icons/circle_from.svg";
+// import fromIcon from "../../assets/from_to_icons/circle_from.svg";
+import fromIconInverse from "../../assets/from_to_icons/circle_from_inverse.svg";
 import Select from "../Select.tsx";
-import dots from "../../assets/from_to_icons/circles_from_to.svg";
-import destination from "../../assets/from_to_icons/icon_to.svg";
+// import dots from "../../assets/from_to_icons/circles_from_to.svg";
+import dotsInverse from "../../assets/from_to_icons/circles_from_to_inverse.svg";
+// import destination from "../../assets/from_to_icons/icon_to.svg";
+import destinationInverse from "../../assets/from_to_icons/icon_to_inverse.svg";
 import { AlgorithmButtons } from "./AlgorithmButtons.tsx";
 import star from "../../assets/Star.svg";
 import Node from "../../../../../packages/common/src/node";
-import { motion, AnimatePresence } from 'framer-motion';
+import {motion, AnimatePresence} from 'framer-motion';
 
 export function PathSelector(props: {
     nodes: Node[],
@@ -53,8 +56,12 @@ export function PathSelector(props: {
         <div className="absolute top-7 flex flex-row-reverse items-center bg-white w-fit rounded-r-xl h-[160px]">
             <button
                 onClick={toggleVisibility}
-                className={"h-full px-5 bg-deep-blue font-OpenSans items-center font-bold text-bone-white rounded-r-xl"}
-            >B</button>
+                className={"h-full px-2 bg-deep-blue font-OpenSans items-center font-bold text-bone-white rounded-r-xl flex flex-col justify-around"}
+            >
+                <img src={fromIconInverse} alt="fromIconInverse" className="mt-3.5"/>
+                <img src={dotsInverse} alt="dotsInverse" className=""/>
+                <img src={destinationInverse} alt="destinationInverse" className="mb-2.5"/>
+            </button>
             <AnimatePresence>
                 {isVisible && (
                     <motion.div
@@ -66,8 +73,8 @@ export function PathSelector(props: {
                         >
 
                             <div
-                                className="grid grid-cols-[auto_1fr] grid-rows-3 h-fit justify-items-center items-center pt-2 pr-2 pl-2">
-                                <img src={fromIcon} alt="from" className="px-1"/>
+                                className="grid grid-cols-1 grid-rows-3 h-fit justify-items-center items-center p-2">
+                                {/*<img src={fromIcon} alt="from" className="px-1"/>*/}
                                 <Select label="" id="nodeStartSelect" options={props.nodes.map((node) => {
                                     return node.nodeID;
                                 })}
@@ -76,9 +83,23 @@ export function PathSelector(props: {
                                         })}
                                         onChange={props.handleStartChange as (e: React.ChangeEvent<HTMLSelectElement>) => void}
                                         defaultOption={props.selectedStartOption !== undefined ? props.selectedStartOption : "Select your start location"}/>
-                                <img src={dots} alt="dots" className="h-7 pb-1 px-1"/>
-                                <div></div>
-                                <img src={destination} alt="destination" className="px-1"/>
+                                {/*<img src={dots} alt="dots" className="px-1"/>*/}
+                                <div
+                                    className="w-full flex flex-row justify-center rounded-br-xl rounded-bl-0 font-OpenSans items-center font-bold text-bone-white">
+                                    <div className="divide-x divide-solid py-2 flex flex-row divide-deep-blue">
+                                        <AlgorithmButtons px="px-8" onClick={props.onClick}
+                                                          isActive={props.selectedAlgo === "Astar"}>
+                                            A <img src={star} alt="A Star Icon" height="15" width="15"/>
+                                        </AlgorithmButtons>
+                                        <AlgorithmButtons px="px-8" onClick={props.onClick1}
+                                                          isActive={props.selectedAlgo === "BFS"}> BFS </AlgorithmButtons>
+                                        <AlgorithmButtons px="px-8" onClick={props.onClick2}
+                                                          isActive={props.selectedAlgo === "DFS"}> DFS </AlgorithmButtons>
+                                        <AlgorithmButtons px="px-8" onClick={props.onClick3}
+                                                          isActive={props.selectedAlgo === "Dijkstra"}> DIJKSTRA </AlgorithmButtons>
+                                    </div>
+                                </div>
+                                {/*<img src={destination} alt="destination" className="px-1"/>*/}
                                 <Select label="" id="nodeEndSelect" options={props.nodes.map((node) => {
                                     return node.nodeID;
                                 })}
@@ -87,21 +108,6 @@ export function PathSelector(props: {
                                         })}
                                         onChange={props.handleEndChange as (e: React.ChangeEvent<HTMLSelectElement>) => void}
                                         defaultOption={props.selectedEndOption !== undefined ? props.selectedEndOption : "Select your end location"}/>
-                            </div>
-                            <div
-                                className="w-full flex flex-row justify-center mt-2 rounded-br-xl rounded-bl-0 font-OpenSans items-center font-bold text-bone-white">
-                                <div className="divide-x divide-solid py-2 flex flex-row divide-deep-blue">
-                                    <AlgorithmButtons px="px-8" onClick={props.onClick}
-                                                      isActive={props.selectedAlgo === "Astar"}>
-                                        A <img src={star} alt="A Star Icon" height="15" width="15"/>
-                                    </AlgorithmButtons>
-                                    <AlgorithmButtons px="px-8" onClick={props.onClick1}
-                                                      isActive={props.selectedAlgo === "BFS"}> BFS </AlgorithmButtons>
-                                    <AlgorithmButtons px="px-8" onClick={props.onClick2}
-                                                      isActive={props.selectedAlgo === "DFS"}> DFS </AlgorithmButtons>
-                                    <AlgorithmButtons px="px-8" onClick={props.onClick3}
-                                                      isActive={props.selectedAlgo === "Dijkstra"}> DIJKSTRA </AlgorithmButtons>
-                                </div>
                             </div>
 
                     </motion.div>)}
