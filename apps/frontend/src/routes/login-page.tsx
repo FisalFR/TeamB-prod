2009da8a476ee83eb6a198855f4e7866fe6444b7
Jeremy Mulia Kurtz <kurtzjeremy126@gmail.com>
2024-04-20 01:15:50 -0400

    Added new Component Library and Button from Aceternity moving-border.tsx

--- apps/frontend/src/routes/login-page.tsx
+++ apps/frontend/src/routes/login-page.tsx
@@ -1,4 +1,4 @@
-import AnimatedSVG from "../components/HeroImage.tsx";
+import AnimatedSVG from "../components/login/HeroImage.tsx";
 
 import React, {useState} from 'react';
 import Carousel from "../components/Carousel";
@@ -8,9 +8,10 @@ import image3 from '../assets/Carousel/security.jpg';
 import image4 from '../assets/Carousel/Map.png';
 import background from '../assets/Hero_Image/brighams_hero.jpg';
 import {useEffect} from "react";
-import ShinyButton from "../components/ShinyButton.tsx";
-import Button from "../components/Button.tsx";
+import ShinyButton from "../components/button/ShinyButton.tsx";
+import Button from "../components/button/Button.tsx";
 import { motion } from "framer-motion";
+import {ButtonMovingBorder} from "../components/aceternity/moving-border.tsx";
 export function LoginPage() {
 
     useEffect(() => {
@@ -23,8 +24,8 @@ export function LoginPage() {
         };
     }, []); // Empty dependency array ensures this runs on mount and unmount only
 
-     const [isHovered,setIsHovered] = useState(true);
-     const content = [
+    const [isHovered,setIsHovered] = useState(true);
+    const content = [
         {
             image: { src: image4, alt: 'Description of image 1' },
             text: {
@@ -63,16 +64,16 @@ export function LoginPage() {
         }
     ];
 
-const style = {
-    backgroundImage: `linear-gradient(to bottom , rgba(21,155,211), rgba(1,45,90,0.3), rgba(1, 45, 90, 1)),url(${background})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    zIndex: 0,
-    top: 0,
-    left: 0,
-    height: '100%',
-};
+    const style = {
+        backgroundImage: `linear-gradient(to bottom , rgba(21,155,211), rgba(1,45,90,0.3), rgba(1, 45, 90, 1)),url(${background})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        zIndex: 0,
+        top: 0,
+        left: 0,
+        height: '100%',
+    };
 
 
 
@@ -86,38 +87,65 @@ const style = {
                     With pathfinding locate your destination in the smallest steps. Never get lost, no wasted time, get
                     there stress free.
                 </p>
-                <div className={"fancy p-5"}>
-                    <a href="/map"
-                       className="text-white text-xl hover:cursor-pointer hover:text-white hover:font-bold pb-5">
-                        Click here to navigate!
-                    </a>
-                    {/*<input type="text" placeholder="Enter your name"/>*/}
+                <div className={"flex flex-col gap-2 centerContent"}>
+                    <motion.div className="pt-5 flex centerContent gap-2 flex-col"
+                                animate={{scale: [1.0, 1.05]}}
+                                transition={{
+                                    duration: 0.4,
+                                    repeat: Infinity,
+                                    repeatType: "reverse",
+                                }}>
 
-                </div>
-                <motion.div className="pt-5"
-                            animate={{scale: [1.0, 1.05]}}
-                            transition={{
-                                duration: 0.4,
-                                repeat: Infinity,
-                                repeatType: "reverse",
-                            }}>
+                        <Button>
+                            <a href="/map"
+                               className=" font-OpenSans font-bold text-white text-xl hover:cursor-pointer hover:text-white hover:font-extrabold pb-5">
+                                Click here to navigate!
+                            </a>
+                        </Button>
 
-                    <Button>
+                        <ButtonMovingBorder
+                            borderRadius="0.3rem"
+                            className="bg-deep-blue text-white border-neutral-200"
+                            containerClassName={"p-[2px] w-60"}
+                        >
+                            <a href="/map"
+                               className=" font-OpenSans font-bold text-white text-xl hover:cursor-pointer hover:text-white">
+                                Click here to navigate!
+                            </a>
+                        </ButtonMovingBorder>
+
+
+                    </motion.div>
+                    <button
+                        className="relative inline-flex h-12 overflow-hidden rounded p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                    >
+                    <span
+                        className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#F6BD38_0%,#FFFFFF_50%,#F6BD38_100%)]"/>
+                        <span
+                            className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded bg-deep-blue px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                         <a href="/map"
-                           className=" font-OpenSans font-bold text-white text-xl hover:cursor-pointer hover:text-white hover:font-extrabold pb-5">
+                           className=" font-OpenSans font-bold text-white text-xl hover:cursor-pointer hover:text-white">
+                            Click here to navigate!
+                        </a></span>
+                    </button>
+                    <ButtonMovingBorder
+                        borderRadius="0.3rem"
+                        className="bg-deep-blue text-white border-neutral-200"
+                        containerClassName={"p-[2px] w-60"}
+                    >
+                        <a href="/map"
+                           className=" font-OpenSans font-bold text-white text-xl hover:cursor-pointer hover:text-white">
                             Click here to navigate!
                         </a>
-                    </Button>
+                    </ButtonMovingBorder>
 
-
-                </motion.div>
-
-
-                <div className={"pt-5"}>
-                    <ShinyButton>
-                        Click Here To Navigate!
-                    </ShinyButton>
+                    <div className={"pt-5"}>
+                        <ShinyButton>
+                            Click Here To Navigate!
+                        </ShinyButton>
+                    </div>
                 </div>
+
             </div>
 
             <div className="pt-20 z-20">
@@ -125,15 +153,15 @@ const style = {
             </div>
 
             <div className="flex justify-center">
-            <div className="overflow-hidden bg-transparent shadow-md rounded-lg border-none hover:cursor-pointer hover:scale-105 transition-transform duration-300"
-                     onMouseEnter={() => setIsHovered(false)}
-                     onMouseLeave={() => setIsHovered(true)}>
+                <div
+                    className="overflow-hidden bg-transparent shadow-md rounded-lg border-none hover:cursor-pointer hover:scale-105 transition-transform duration-300"
+                    onMouseEnter={() => setIsHovered(false)}
+                    onMouseLeave={() => setIsHovered(true)}>
                     <Carousel content={content} autoPlay={isHovered} interval={4000}/>
                 </div>
             </div>
         </div>
     );
-
 
 
 }
