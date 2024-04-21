e3964bae6f2183678ac2a88a987fbb8410a9e8c1
Benjamin Cruse <57077532+TheBoopleBeep@users.noreply.github.com>
2024-04-21 16:36:03 -0400

    fixed directions for js element

--- apps/frontend/src/components/map/pathInstructions.tsx
+++ apps/frontend/src/components/map/pathInstructions.tsx
@@ -2,13 +2,19 @@ import Node from "common/src/node.ts";
 import {Instruction} from "common/src/instruction.ts";
 import React from "react";
 import genInstructions from "backend/src/directionTools.ts";
-export default function pathInstructions(props:{Path: Node[]}) {
-    const [instructions, setInstructions] = React.useState<Instruction[]>([]);
 
-    function createInstruction() {
-        let Instructions = genInstructions(props.Path);
+function PathInstructions(props: { Path: Node[] }) {
+    const Instructions: Instruction[] = genInstructions(props.Path);
 
-
-    };
-
+    return (
+        <div>
+            {Instructions.map(instruction => (
+                <div>
+                    {instruction.type} {instruction.content}
+                </div>
+            ))}
+        </div>
+    );
 }
+
+export default PathInstructions;
