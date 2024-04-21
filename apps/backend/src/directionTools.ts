6d003ccb3dc0bce262931acac088ea2567c04c58
Benjamin Cruse <57077532+TheBoopleBeep@users.noreply.github.com>
2024-04-21 17:48:40 -0400

    refactored and removed a variable

--- apps/backend/src/directionTools.ts
+++ apps/backend/src/directionTools.ts
@@ -1,11 +1,12 @@
-import Node from "common/src/node";
-import {Instruction} from "common/src/instruction";
+import Node from 'common/src/node';
+import {Instruction} from 'common/src/instruction';
+import {useState} from "react";
 const distThresh = 40;
 const turnThresh = Math.PI / 3;
 const backTrackThresh = Math.PI/4;
 
  function dist(node1: Node, node2: Node):number {
-    return Math.sqrt(Math.pow(node1.xcoord - node2.xcoord, 2) + Math.pow(node1.ycoord - node2.ycoord, 2))
+    return Math.sqrt(Math.pow(node1.xcoord - node2.xcoord, 2) + Math.pow(node1.ycoord - node2.ycoord, 2));
 }
 
  function angle(node1: Node, node2: Node):number {
@@ -19,7 +20,7 @@ function angleDiff(angle1:number, angle2:number):number {
 function detectBranch(compAngle:number, branchNode:Node, curNode:Node):boolean  {
     let turn = false;
     for (const testNode of curNode.neighbors ) {
-        if (testNode != branchNode && testNode.nodeType == "hall" ){
+        if (testNode != branchNode && testNode.nodeType == `hall` ){
             if (Math.abs(angleDiff(compAngle + Math.PI, angle(testNode,curNode))) < backTrackThresh){
                 continue;
             }
@@ -36,41 +37,44 @@ function detectBranch(compAngle:number, branchNode:Node, curNode:Node):boolean  
     return turn;
 }
 
-function pathTurn(path:Node[],index:number){
+function pathTurn(path:Node[],index:number):string{
     let offset=1;
     if(index==0)
-        return "Start";
+        return `Start`;
     while(index < path.length){
         if (dist(path[index],path[index+offset]) > distThresh){
-            const dirDiff = angleDiff(angle(path[index],path[index+offset]),angle(path[index+offset-1],path[index+offset]));
+            const dirDiff = angleDiff(angle(path[index-1],path[index]),angle(path[index+offset-1],path[index+offset]));
             if ( dirDiff > turnThresh){
                 if (dirDiff<0)
-                    return "Right";
-                else return "Left";
+                    return `Right`;
+                else return `Left`;
             }
-            else return "Forward";
+            else return `Forward`;
         }
         offset++;
     }
-    return "End";
+    return `End`;
 }
 
-export default function genInstructions(path:Node[]){
+export default function genInstructions(path:Node[]):Instruction[]{
+    if (path.length<2)
+        return [];
     let index = 1;
     const instructions:Instruction[] =[];
-    const placeHolder:Instruction = {type:"Start",content: "Starting from "+path[0].shortName};
-    instructions.push(placeHolder);
+    const startString=`Starting from ${path[0].shortName}`;
+    instructions.push({type:`Start`,content:startString });
     let amtIntersections = 0;
     while (index < path.length-1){
-        placeHolder.type = pathTurn(path,index);
-        if (placeHolder.type == "Right" || placeHolder.type == "Left"){
+        const turn = pathTurn(path,index);
+        let content:string;
+        if (turn == `Right` || turn == `Left`){
             if (amtIntersections == 0){
-                placeHolder.content="Walk down the hallway and take the first hallway on the "+placeHolder.type+".";
+                content=`Walk down the hallway and take the first hallway on the ${turn}.`;
             }
             else
-                placeHolder.content="Walk past "+amtIntersections+" hallways and take the following hallway on the "+placeHolder.type+".";
+                content=`Walk past ${amtIntersections} hallways and take the following hallway on the ${turn}.`;
             amtIntersections=0;
-            instructions.push(placeHolder);
+            instructions.push({type:turn,content:content});
         }
         else{
             if (detectBranch(angle(path[index-1],path[index]),path[index],path[index]))
@@ -78,24 +82,24 @@ export default function genInstructions(path:Node[]){
         }
         index++;
     }
-    placeHolder.type = "End";
+    let content:string;
     if (amtIntersections==0)
-        placeHolder.content="Walk down the hallway ";
-    else placeHolder.content=" Walk past "+amtIntersections+" hallways ";
+        content=`Walk down the hallway `;
+    else content=` Walk past ${amtIntersections} hallways `;
     const finalDir = pathTurn(path,path.length-2);
     switch (finalDir) {
-        case "Forward":
-        placeHolder.content += " and your destination is at the end of the hallway.";
+        case `Forward`:
+        content = `${content} and your destination is at the end of the hallway.`;
         break;
-        case "Left":
-            placeHolder.content += " and your destination is on the left.";
+        case `Left`:
+            content = `${content} and your destination is on the left.`;
             break;
-        case "Right":
-            placeHolder.content += " and your destination is on the right.";
+        case `Right`:
+            content = `${content} and your destination is on the right.`;
             break;
         default:
-            placeHolder.content+="and you will arrive at your destination";
+            content=`${content} and you will arrive at your destination`;
     }
-    instructions.push(placeHolder);
+    instructions.push({type:"End",content:content});
     return instructions;
 }
