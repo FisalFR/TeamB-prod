f85e73ffa985211d374e0c32c96bb4be79e958bc
Lily Jones <127898963+Lily7jones@users.noreply.github.com>
2024-04-22 13:01:32 -0400

    Finish styling

--- apps/frontend/src/routes/transportationRequest.tsx
+++ apps/frontend/src/routes/transportationRequest.tsx
@@ -101,14 +101,14 @@ export function TransportationRequestPage() {
                             }} className="">
                                 <div className="flex justify-between">
                                     <div className="flex flex-col">
-                                        <p className="font-bold">Employee Name</p>
+                                        <p className=" text-left font-bold">Employee Name</p>
                                         <input type="text" required
                                                placeholder={"Name"}
                                                onChange={handleNameInput} value={request.employeeName}
                                                className={"border-solid border-deep-blue border-2 rounded overflow-hidden flex items-start p-[5px] w-80"}/>
                                     </div>
                                     <div className="flex flex-col ">
-                                        <p className={"font-bold"}>What room is the patient in?</p>
+                                        <p className={"text-left font-bold"}>What room is the patient in?</p>
                                         <div className="border-deep-blue border-solid border-2 rounded w-fit">
                                             <Dropdown options={locationOptions} placeholder={"Location"}
                                                       name={"locationDropdown"}
@@ -121,23 +121,30 @@ export function TransportationRequestPage() {
                                 <div className="flex justify-between">
                                     <div>
                                         <p className={"text-left font-bold"}>How will the patient be transported?</p>
-                                        <div className={"w-80 border-solid border-deep-blue border-2 rounded"}>
-                                            <RadioButton value={"Ambulance"} name={"transport"} id={"transport1"}
-                                                         state={request.transport}
-                                                         onChange={handleTransportInput} required={true} width={"w-80"}/>
-                                            <RadioButton value={"Helicopter"} name={"transport"} id={"priority2"}
-                                                         state={request.transport}
-                                                         onChange={handleTransportInput} required={true} width={"w-80"}/>
-                                            <RadioButton value={"Friends/Family/Uber"} name={"priority"} id={"priority3"}
-                                                         state={request.transport}
-                                                         onChange={handleTransportInput} required={true} width={"w-80"}/>
-                                            <RadioButton value={"Non-Emergency Accessible Van"} name={"priority"} id={"priority4"}
-                                                         state={request.transport}
-                                                         onChange={handleTransportInput} required={true} width={"w-80"}/>
+                                        <div>
+                                            <select onChange={handleTransportInput} className="w-80 border-solid border-deep-blue border-2 p-[5px] rounded" name="transportation">
+                                                <option value={"placeholder"}>Select Mode of Transport</option>
+                                                <option value={"Ambulance"} state={request.transport}>Ambulance</option>
+                                                <option value={"Helicopter"}>Helicopter</option>
+                                                <option value={"Friends/Family/Uber"}>Friends/Family/Rideshare</option>
+                                                <option value={"Non-Emergency Accessible Van"}>Non-Emergency Accessible Van</option>
+                                            </select>
+                                            {/*<RadioButton value={"Ambulance"} name={"transport"} id={"transport1"}*/}
+                                            {/*             state={request.transport}*/}
+                                            {/*             onChange={handleTransportInput} required={true} width={"w-80"}/>*/}
+                                            {/*<RadioButton value={"Helicopter"} name={"transport"} id={"priority2"}*/}
+                                            {/*             state={request.transport}*/}
+                                            {/*             onChange={handleTransportInput} required={true} width={"w-80"}/>*/}
+                                            {/*<RadioButton value={"Friends/Family/Uber"} name={"priority"} id={"priority3"}*/}
+                                            {/*             state={request.transport}*/}
+                                            {/*             onChange={handleTransportInput} required={true} width={"w-80"}/>*/}
+                                            {/*<RadioButton value={"Non-Emergency Accessible Van"} name={"priority"} id={"priority4"}*/}
+                                            {/*             state={request.transport}*/}
+                                            {/*             onChange={handleTransportInput} required={true} width={"w-80"}/>*/}
                                         </div>
                                     </div>
                                     <div>
-                                        <p className="font-bold">Drop off Location Address</p>
+                                        <p className="text-left font-bold">Drop off Location Address</p>
                                         <input type="text" required
                                                placeholder={"Address"}
                                                onChange={handleAddressInput} value={request.address}
@@ -151,24 +158,24 @@ export function TransportationRequestPage() {
                                         <div className={"w-80 border-solid border-deep-blue border-2 rounded"}>
                                             <RadioButton value={"Low"} name={"priority"} id={"priority1"}
                                                          state={request.priority}
-                                                         onChange={handlePriorityInput} required={true} width={"w-80"}/>
+                                                         onChange={handlePriorityInput} required={true} width={"w-76"}/>
                                             <RadioButton value={"Medium"} name={"priority"} id={"priority2"}
                                                          state={request.priority}
-                                                         onChange={handlePriorityInput} required={true} width={"w-80"}/>
+                                                         onChange={handlePriorityInput} required={true} width={"w-76"}/>
                                             <RadioButton value={"High"} name={"priority"} id={"priority3"}
                                                          state={request.priority}
-                                                         onChange={handlePriorityInput} required={true} width={"w-80"}/>
+                                                         onChange={handlePriorityInput} required={true} width={"w-76"}/>
                                             <RadioButton value={"Emergency"} name={"priority"} id={"priority4"}
                                                          state={request.priority}
-                                                         onChange={handlePriorityInput} required={true} width={"w-80"}/>
+                                                         onChange={handlePriorityInput} required={true} width={"w-76"}/>
                                         </div>
                                     </div>
                                     <div>
-                                        <p className="font-bold">Additional Comments </p>
+                                        <p className=" text-left font-bold">Additional Comments </p>
                                         <textarea id={"feedback"}
                                                   className={"w-80 max-w-full h-40 max-h-40 p-1 border-deep-blue border-solid border-2 rounded"}
                                                   onChange={handleFeedbackInput}
-                                                  value={request.feedback} required={true}
+                                                  value={request.feedback} required={false}
                                                   placeholder="Enter detailed description here..."/>
                                     </div>
                                 </div>
