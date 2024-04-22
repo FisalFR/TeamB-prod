cfca4858718850a9d43e0141ce9f7fc10f4e35f8
tacheampong <th.acheampong@gmail.com>
2024-04-22 18:09:30 -0400

    got rid of comments

--- apps/frontend/src/components/NavigationBar.tsx
+++ apps/frontend/src/components/NavigationBar.tsx
@@ -35,39 +35,13 @@ export function NavigationBar() {
 
         return (
             <div>
-                {/*{ (!isLoading && isAuthenticated) ? (*/}
-                {/*    <>*/}
-                {/*        <div className="navbar z-50 relative bg-deep-blue static h-14 top-0 left-0 grid w-full">*/}
-                {/*            <img onClick={home} className="hover:cursor-pointer h-3/6 self-center px-4" src={bwhLogo}*/}
-                {/*                 alt="Brighams Logo White"></img>*/}
-                {/*            <nav className="uppercase divide-x divide-solid centerContent w-fit justify-self-center">*/}
 
-                {/*                <NavDropDown mainLink= {["", "Request"]} dropdownLinks={dropdownList}/>*/}
-
-                {/*                <div className="px-16">*/}
-                {/*                    <NavLink href="/map">Map</NavLink>*/}
-                {/*                </div>*/}
-
-                {/*                <NavDropDown mainLink={["", "Admin"]} dropdownLinks={adminDropdownList}></NavDropDown>*/}
-                {/*            </nav>*/}
-                {/*            <div className="self-center">*/}
-                {/*                <div className="float-end centerContent px-16">*/}
-
-                {/*                    <NavLink onClick={handleLogout}><div className="flex flex-row gap-2">*/}
-                {/*                        <img src={user_icon} alt="Username icon" height="20" width="20"/>LOGOUT</div></NavLink>*/}
-                {/*                </div>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    </>*/}
-                {/*): (*/}
-                {/*    <LoginNavigationBar/>*/}
-                {/*)}*/}
                 {
                     (!isLoading && isAuthenticated && window.location.href !== homeRedirect)
                         ? (
                             // Output when user is authenticated and page is not loading
                             <>
-                                <div className="navbar z-50 relative bg-deep-blue static h-14 top-0 left-0 grid w-full">
+                                <div className="navbar z-50 relative bg-deep-blue h-14 top-0 left-0 grid w-full">
                                     <img onClick={home} className="hover:cursor-pointer h-3/6 self-center px-4" src={bwhLogo}
                                          alt="Brighams Logo White"></img>
                                     <nav className="uppercase divide-x divide-solid centerContent w-fit justify-self-center">
