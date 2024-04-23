7fb5de9048e52b3c424bf6bd1e3918f6fc950a91
mominhnguyen <61259039+mominhnguyen@users.noreply.github.com>
2024-04-23 18:21:47 -0400

    Refixed Astar again???

--- packages/common/src/Pathfinding/AStarStrategy.ts
+++ packages/common/src/Pathfinding/AStarStrategy.ts
@@ -4,99 +4,127 @@ import TemplatePathfindingStrategy from "./TemplatePathfindingStrategy.ts";
 
 class AStarStrategy extends TemplatePathfindingStrategy {
 
-    heuristic(endNode: Node, nextNode: Node, currentNode:Node): number {
+    // heuristic(endNode: Node, nextNode: Node, currentNode:Node): number {
+    //     const endFloor: number  = this.convertFloor(endNode.floor);
+    //     const nextFloor: number = this.convertFloor(nextNode.floor);
+    //     const EuclideanDistance = Math.sqrt((endNode.ycoord - nextNode.ycoord) ** 2 + (endNode.xcoord - nextNode.xcoord) ** 2);
+    //     // const DistToElevL: number = Math.sqrt((924 -nextNode.ycoord) ** 2 + (1785 - nextNode.xcoord) ** 2);
+    //     const DistToElevQ: number = Math.sqrt((1825 -nextNode.ycoord) ** 2 + (1751 - nextNode.xcoord) ** 2);
+    //     const floorDifference = Math.abs(endFloor - nextFloor);
+    //     // let finalCost = EuclideanDistance;
+    //
+    //     // If I'm not in the same building as the end node, and across the bridge, prioritize the bridge
+    //     if ((nextNode.building !== endNode.building) && (endNode.building === "Shapiro" || endNode.building === "BTM") ){
+    //         if((nextFloor === 1 && endFloor == 1) && endNode.building === "Shapiro"){
+    //             return EuclideanDistance/2;
+    //         } else if(nextFloor === 4 || (nextFloor === 4 && endNode.building === "BTM")){
+    //             return EuclideanDistance/2;
+    //         }
+    //     } else if ((currentNode.building === "Shapiro" || currentNode.building === "BTM") && (nextNode.building !== endNode.building) && (endFloor >=3)){
+    //         if(nextFloor === 4){
+    //             return EuclideanDistance/2;
+    //         }
+    //     } else if ((currentNode.building === "BTM") && (nextNode.building !== endNode.building) && (endFloor <3)){
+    //         if(nextFloor === 4){
+    //             return EuclideanDistance/2;
+    //         }
+    //     }
+    //
+    //     // If the end ndoe is in L1 Shapiro and we're in Shapiro, prioritize Elevator Q
+    //     if (endNode.building === "Shapiro" && nextNode.building === "Shapiro" && endNode.floor === "L1"){
+    //         if(nextNode.nodeType === "ELEV" && nextNode.longName.includes("Elevator Q")){
+    //             return 0;
+    //         } else if (nextNode.nodeType === "ELEV" && !nextNode.longName.includes("Elevator Q")){
+    //             return 10000;
+    //         } else {
+    //             return DistToElevQ;
+    //         }
+    //     }
+    //
+    //     // if(endNode.building === "Tower" && endNode.floor ==="3" && currentNode.floor === "3"){
+    //     //     if (nextFloor === 4){
+    //     //         return 0;
+    //     //     }
+    //     // }
+    //     //
+    //     // // If the node is in the Tower, and we're not in the right floor, get to Elevator L
+    //     // if(endNode.building === "Tower" && endNode.floor !== currentNode.floor) {
+    //     //     if (nextNode.nodeType === "ELEV" && nextNode.shortName.includes("Elevator L")) {
+    //     //         return 0;
+    //     //     } else if (nextNode.nodeType === "ELEV" && !nextNode.shortName.includes("Elevator L")) {
+    //     //         return 10000;
+    //     //     } else {
+    //     //         return DistToElevL;
+    //     //     }
+    //     // }
+    //
+    //     // If we approach an ELEVATOR, prioritize if we're on the wrong floor
+    //     // Otherwise,  DON'T TAKE THE ELEVATOR
+    //     if((nextNode.nodeType === "ELEV" && currentNode.nodeType !== "ELEV")){
+    //         if(nextFloor !== endFloor){
+    //             return EuclideanDistance - floorDifference + 10000;
+    //         } else if (nextFloor === endFloor){
+    //             return EuclideanDistance + 10 + 10000;
+    //         }
+    //     } else if ((nextNode.nodeType === "ELEV" && currentNode.nodeType === "ELEV")){
+    //         if(nextFloor === endFloor){
+    //             return 10000;
+    //         } else if (nextFloor !== endFloor){
+    //             return EuclideanDistance + 100000 + (floorDifference * 100) + 10000;
+    //         }
+    //     }
+    //
+    //     // If we approach a STAIR, prioritize if we're on the wrong floor
+    //     // Otherwise,  DON'T TAKE THE STAIRS
+    //     if((nextNode.nodeType === "STAI" && currentNode.nodeType !== "STAI")){
+    //         if(nextFloor !== endFloor && floorDifference === 1){
+    //             return EuclideanDistance - (floorDifference/2) + 10000;
+    //         } else if (nextFloor === endFloor){
+    //             return EuclideanDistance + 10 + 10000;
+    //         }
+    //     } else if ((nextNode.nodeType === "STAI" && currentNode.nodeType === "STAI")){
+    //         if(nextFloor === endFloor){
+    //             return 10000;
+    //         } else if (nextFloor !== endFloor){
+    //             return EuclideanDistance + 100000 + 10000;
+    //         }
+    //     }
+    //
+    //     if (endFloor === nextFloor) {
+    //         return EuclideanDistance;
+    //     }
+    //
+    // }
+
+    heuristic(endNode: Node, nextNode: Node, currentNode: Node): number {
         const endFloor: number  = this.convertFloor(endNode.floor);
         const nextFloor: number = this.convertFloor(nextNode.floor);
-        const EuclideanDistance = Math.sqrt((endNode.ycoord - nextNode.ycoord) ** 2 + (endNode.xcoord - nextNode.xcoord) ** 2 + ((endFloor - nextFloor) * 100) ** 2);
-        const DistToElevL: number = Math.sqrt((924 -nextNode.ycoord) ** 2 + (1785 - nextNode.xcoord) ** 2);
+        const EuclideanDistance = Math.sqrt((endNode.ycoord - nextNode.ycoord) ** 2 + (endNode.xcoord - nextNode.xcoord) ** 2) + (((endFloor-nextFloor) * 100)**2);
+        // const DistToElevL: number = Math.sqrt((924 -nextNode.ycoord) ** 2 + (1785 - nextNode.xcoord) ** 2);
         const DistToElevQ: number = Math.sqrt((1825 -nextNode.ycoord) ** 2 + (1751 - nextNode.xcoord) ** 2);
         const floorDifference = Math.abs(endFloor - nextFloor);
-        let finalCost = EuclideanDistance;
+        // let finalCost = EuclideanDistance;
 
         // If I'm not in the same building as the end node, and across the bridge, prioritize the bridge
-        if ((nextNode.building !== endNode.building) && (endNode.building === "Shapiro" || endNode.building === "BTM") ){
-            if((nextFloor === 1 && endFloor == 1) && endNode.building === "Shapiro"){
-                return EuclideanDistance/2;
-            } else if(nextFloor === 4 || (nextFloor === 4 && endNode.building === "BTM")){
-                return EuclideanDistance/2;
+            if ((nextNode.building !== endNode.building) && (endNode.building === "Shapiro" || endNode.building === "BTM") ){
+                if((nextFloor === 1 && endFloor == 1) && endNode.building === "Shapiro"){
+                    return EuclideanDistance/100000;
+                } else if(nextFloor === 4 || (nextFloor === 4 && endNode.building === "BTM")){
+                    return EuclideanDistance/100000;
+                } else if(nextFloor !==4){
+                    return 100000000000000000000;
+                }
+            } else if ((currentNode.building === "Shapiro" || currentNode.building === "BTM") && (nextNode.building !== endNode.building) && (endFloor >=3)){
+                if(nextFloor === 4){
+                    return EuclideanDistance/100000;
+                }
+            } else if ((currentNode.building === "BTM") && (nextNode.building !== endNode.building) && (endFloor <3)){
+                if(nextFloor === 4){
+                    return EuclideanDistance/100000;
+                }
             }
-        } else if ((currentNode.building === "Shapiro" || currentNode.building === "BTM") && (nextNode.building !== endNode.building) && (endFloor >=3)){
-            if(nextFloor === 4){
-                return EuclideanDistance/2;
-            }
-        } else if ((currentNode.building === "BTM") && (nextNode.building !== endNode.building) && (endFloor <3)){
-            if(nextFloor === 4){
-                return EuclideanDistance/2;
-            }
-        }
 
-        // If the end ndoe is in L1 Shapiro and we're in Shapiro, prioritize Elevator Q
-        if (endNode.building === "Shapiro" && nextNode.building === "Shapiro" && endNode.floor === "L1"){
-            if(nextNode.nodeType === "ELEV" && nextNode.longName.includes("Elevator Q")){
-                return 0;
-            } else if (nextNode.nodeType === "ELEV" && !nextNode.longName.includes("Elevator Q")){
-                return 10000;
-            } else {
-                return DistToElevQ;
-            }
-        }
-
-        // if(endNode.building === "Tower" && endNode.floor ==="3" && currentNode.floor === "3"){
-        //     if (nextFloor === 4){
-        //         return 0;
-        //     }
-        // }
-        //
-        // // If the node is in the Tower, and we're not in the right floor, get to Elevator L
-        // if(endNode.building === "Tower" && endNode.floor !== currentNode.floor) {
-        //     if (nextNode.nodeType === "ELEV" && nextNode.shortName.includes("Elevator L")) {
-        //         return 0;
-        //     } else if (nextNode.nodeType === "ELEV" && !nextNode.shortName.includes("Elevator L")) {
-        //         return 10000;
-        //     } else {
-        //         return DistToElevL;
-        //     }
-        // }
-
-        // If we approach an ELEVATOR, prioritize if we're on the wrong floor
-        // Otherwise,  DON'T TAKE THE ELEVATOR
-        if((nextNode.nodeType === "ELEV" && currentNode.nodeType !== "ELEV")){
-            if(nextFloor !== endFloor){
-                return EuclideanDistance - floorDifference;
-            } else if (nextFloor === endFloor){
-                return EuclideanDistance + 10;
-            }
-        } else if ((nextNode.nodeType === "ELEV" && currentNode.nodeType === "ELEV")){
-            if(nextFloor === endFloor){
-                return 0;
-            } else if (nextFloor !== endFloor){
-                return EuclideanDistance + 100000 + (floorDifference * 100);
-            }
-        }
-
-        // If we approach a STAIR, prioritize if we're on the wrong floor
-        // Otherwise,  DON'T TAKE THE STAIRS
-        if((nextNode.nodeType === "STAI" && currentNode.nodeType !== "STAI")){
-            if(nextFloor !== endFloor && floorDifference === 1){
-                return EuclideanDistance - (floorDifference/2);
-            } else if (nextFloor === endFloor){
-                return EuclideanDistance + 10;
-            }
-        } else if ((nextNode.nodeType === "STAI" && currentNode.nodeType === "STAI")){
-            if(nextFloor === endFloor){
-                return 0;
-            } else if (nextFloor !== endFloor){
-                return EuclideanDistance + 100000;
-            }
-        }
-
-        if (endFloor === nextFloor){
-            return EuclideanDistance;
-        } else if (endFloor !== nextFloor){
-            return EuclideanDistance + 10000;
-        }
-
-        return EuclideanDistance+ 10;
+        return EuclideanDistance;
 
     }
 
