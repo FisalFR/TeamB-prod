045f9f1d54322992fe157e79b16e274e3750d9e9
Benjamin Cruse <57077532+TheBoopleBeep@users.noreply.github.com>
2024-04-23 20:55:41 -0400

    changed a buncha stuff

--- apps/frontend/src/components/map/PathDirections.tsx
+++ apps/frontend/src/components/map/PathDirections.tsx
@@ -1,6 +1,32 @@
 import {SideTab} from "./SideTab.tsx";
+import useNodes from "../../hooks/useNodes.ts";
+import useEdges from "../../hooks/useEdges.ts";
+import genInstructions from "../../directionTools.ts";
+import forwardArrow from "../../assets/Forward.svg";
+import leftArrow from "../../assets/LeftArrow.svg";
+import rightArrow from "../../assets/RightArrow.svg";
+import elevator from "../../assets/Elevator.svg";
+import React from "react";
+import Node from "common/src/node.ts";
 
-export function PathDirections(){
+function icon(image:string){
+    switch(image){
+        case "Forward":
+        return (<img src={forwardArrow} alt="FWD"/>);
+        case "Left":
+            return (<img src={leftArrow} alt="LFT"/>);
+        case "Right":
+            return (<img src={rightArrow} alt="RGT"/>);
+        case "Elevator":
+        case "Stairs":
+            return (<img src={elevator} alt="ELV"/>);
+    }
+}
+
+export function PathDirections(props: { Path: Node[]}){
+    const {nodeMap} = useNodes();
+    const {edgeMap} = useEdges();
+
     return (
         <SideTab height={"h-[172px]"} yPos={"top-56"} arrow={false}
             tabChildren={
@@ -8,9 +34,23 @@ export function PathDirections(){
                     <b style={{color: "white"}}>PD</b>
                 </div>}
             bodyChildren={
-                <div>
-                    <h1 style={{color: "#012D5A"}}>PATH<br/>DIRECTIONS</h1>
-                </div>
-        }/>
-    );
-}
+            <div className="h-[132px] overflow-y-scroll overflow-hidden w-[415px]">
+                    { genInstructions(props.Path, nodeMap,edgeMap ).map(instruction => (
+                        <div className=" flex flex-col  text-sm/[17px] gap-36">
+                        <div className="  flex flex-row py-3 ">
+                            <div className="object-left w-1/4 px-5">
+                            <div className=" centerContent ">
+                            {icon(instruction.type)}
+                            </div>
+                            </div>
+
+                            <h1 className="w-3/4 align-middle" style={{color: "#012D5A", fontSize: 17, textAlign:"left"}}>
+                                {instruction.content}
+                            </h1>
+                        </div>
+                        </div>))}
+                        </div>
+
+                    }/>);
+
+            }
--- apps/frontend/src/directionTools.ts
+++ apps/frontend/src/directionTools.ts
@@ -1,11 +1,10 @@
 import Node from 'common/src/node.ts';
 import {Instruction} from 'common/src/instruction.ts';
-const distThresh = 90;
+const distThresh = 40;
 const nearThresh = 3;
-const turnThresh = Math.PI / 2.5;
-// const backTrackThresh = Math.PI/2.5;
-const tooShort = 5.5;
-const endRange = 150;
+const turnThresh = Math.PI / 2.1;
+const tooShort = 4;
+const minSeg = 10;
 const pix2meters = .05;
 
  function dist(node1: Node, node2: Node):number {
@@ -49,22 +48,17 @@ function angleDiff(angle1:number, angle2:number):number {
 function pathTurn(path:Node[],index:number):[string,number]{
     let offset=1;
 
-    if (path[index].nodeType == "ELEV"){
-        return ["Elevator",index];
-    }
-    else if (path[index].nodeType == "STAI"){
-        return ["Stairs",index];
-    }
-    if (dist(path[index],path[path.length-1])<endRange)
-        return ["End",index];
+
+    // if (dist(path[index],path[path.length-1])<endRange)
+    //     return ["End",index];
 
     if(index==0){
         return [`Start`,index];}
-    while(index+offset < path.length-1){
+    while(index+offset < path.length){
         // const imaginary = path[index];
         // imaginary.ycoord = path[index].ycoord+Math.sin(angle(path[index-1],path[index]))*dist(path[index],path[index+offset]);
         // imaginary.xcoord = path[index].xcoord+Math.cos(angle(path[index-1],path[index]))*dist(path[index],path[index+offset]);
-        if ( dist(path[index],path[index+offset])>distThresh ){
+        if ( ((dist(path[index],path[index+offset])>distThresh) && dist(path[index],path[index+1])>minSeg) || (path[index+offset].nodeType=="ELEV" ||  path[index+offset].nodeType=="STAI")){
             const dirDiff = angleDiff(angle(path[index-1],path[index]),angle(path[index+offset-1],path[index+offset]));
             if ( Math.abs(dirDiff )> turnThresh){
                 if (dirDiff>0)
@@ -88,50 +82,64 @@ export default function genInstructions(path:Node[],nodemap: Map<string,Node>, e
         instructions.push({type:"End",content:"You are already near your destination"});
         return instructions;
     }
-    let index = 1;
-    let content = `Starting from ${path[0].shortName}`;
+    let index = 0;
+    instructions.push({type: "Star",content:`Starting from ${path[0].shortName}`});
         for (const neighbor of edgeMap.get(path[3].nodeID)!){
             const compNode = nodemap.get(neighbor)!;
-            if (compNode.nodeType != "HALL" && compNode.nodeType != "ELEV" && compNode.nodeType != "STAI" && compNode.nodeType != "WALK"){
-                content=`Starting from ${path[0].shortName}, turn towards ${compNode.shortName}`;
+
+            if (compNode.nodeType != "HALL" && compNode.nodeType != "ELEV" && compNode.nodeType != "STAI" && compNode.nodeType != "WALK" && dist(path[3],compNode)*pix2meters<nearThresh){
+                instructions.push({type:"Right",content:`Turn towards ${compNode.shortName}`});
                 break;}}
 
-        instructions.push({type:`Start`,content:content});
     //let amtIntersections = 0;
     let prevTurn = path[0];
     let wait = 0;
     let turn;
+    let leftElevator=false;
     while (index < path.length-1){
+        if (index!=0 && (path[index].nodeType == "ELEV" || path[index].nodeType == "STAI") && (path[index-1].nodeType == "ELEV" || path[index-1].nodeType == "STAI")){
+            index++;
+            continue;}
         let content:string;
         if (index>=wait)
             [turn,wait] = pathTurn(path,index);
         if ((turn == `Right` || turn == `Left`)){
-            //
-            // if (amtIntersections == 0){
-            if (dist(prevTurn,path[index])*pix2meters > tooShort){
-                content=`Walk ${Math.round(dist(prevTurn,path[index])*pix2meters)} meters and take a ${turn} turn.`;
+            if (dist(prevTurn,path[index])*pix2meters > tooShort || leftElevator){
+                leftElevator=false;
+                content=`Walk ${Math.round(dist(prevTurn,path[index])*pix2meters)} meters.`;
                 for (const neighbor of edgeMap.get(path[3].nodeID)!){
                     const compNode = nodemap.get(neighbor)!;
                     if (compNode.nodeType != "HALL" && compNode.nodeType != "ELEV" && compNode.nodeType != "STAI" && compNode.nodeType != "WALK" && dist(path[index],compNode)*pix2meters<nearThresh){
-                        content=`Walk ${Math.round(dist(prevTurn,path[index])*pix2meters)} meters to ${compNode.shortName} and take a ${turn} turn.`;
+                        content=`Walk ${Math.round(dist(prevTurn,path[index])*pix2meters)} meters to ${compNode.shortName}. `;
                         break;}}
-
-
-                 instructions.push({type:turn,content:content});
+                 instructions.push({type:"Forward",content:content});
+                instructions.push({type:turn,content:`Take a ${turn.toLowerCase()} turn.`});
             prevTurn=path[index];}
         }
-        else if ((turn=="Elevator" || turn =="Stair") && path[index].floor!=path[index+1].floor){
-            let ogfloor = parseInt(path[index].floor);
+         if ((path[index].nodeType == "ELEV" || path[index].nodeType == "STAI") && path[index].floor!=path[index+1].floor){
+            let ogfloor ;
             if (path[index].floor == "L1")
                 ogfloor = -1;
             else if (path[index].floor == "L2")
                 ogfloor = -2;
-            let newfloor = parseInt(path[index].floor);
-            if (path[index].floor == "L1")
+            else ogfloor = parseInt(path[index].floor);
+            let newfloor ;
+            if (path[index+1].floor == "L1")
                 newfloor = -1;
-            else if (path[index].floor == "L2")
+            else if (path[index+1].floor == "L2")
                 newfloor = -2;
-            instructions.push({type:turn,content:`Take the ${turn.toLowerCase()} ${newfloor>=ogfloor?`up`:`down`} to floor ${path[index+1].floor}`});
+            else newfloor = parseInt(path[index+1].floor);
+            let direction;
+            if (newfloor > ogfloor){ direction = 'up';}
+            else  direction='down';
+            content=(`Take the ${path[index].nodeType=="ELEV"?"elevator":"stairs"} ${direction} to floor ${path[index+1].floor}`);
+            for (const neighbor of edgeMap.get(path[index+3].nodeID)!){
+                const compNode = nodemap.get(neighbor)!;
+                if (compNode.nodeType != "HALL" && compNode.nodeType != "ELEV" && compNode.nodeType != "STAI" && compNode.nodeType != "WALK" && dist(path[index],compNode)*pix2meters<nearThresh){
+                    content=`Take the ${path[index].nodeType=="ELEV"?"elevator":"stairs"} ${newfloor>ogfloor?`up`:`down`} to floor ${path[index+2].floor} and turn towards ${compNode.shortName}.`;
+                    break;}}
+            leftElevator=false;
+            instructions.push({type:path[index].nodeType=="ELEV"?"Elevator":"Stairs",content:content});
         }
         // else if (detectBranch(angle(path[index-1],path[index]),path[index],path[index]))
         //         amtIntersections++;
@@ -139,22 +147,26 @@ export default function genInstructions(path:Node[],nodemap: Map<string,Node>, e
         index++;
     }
     // if (amtIntersections==0)
-         content=`Walk ${Math.round(dist(prevTurn,path[index]) * pix2meters)} meters`;
+        let content=`Walk ${Math.round(dist(prevTurn,path[index]) * pix2meters)} meters`;
     // else content=` Walk past ${amtIntersections} hallways `;
-    const finalDir = pathTurn(path,path.length-3)[0];
+    const finalDir = pathTurn(path,path.length-2)[0];
     switch (finalDir) {
         case `Forward`:
         content = `${content} and your destination is at the end of the hallway.`;
+            instructions.push({type:"Forward",content:content});
         break;
         case `Left`:
             content = `${content} and your destination is on the left.`;
+            instructions.push({type:"Left",content:content});
             break;
         case `Right`:
             content = `${content} and your destination is on the right.`;
+            instructions.push({type:"Right",content:content});
             break;
         default:
             content=`${content} and you will arrive at your destination`;
+            instructions.push({type:"Forward",content:content});
     }
-    instructions.push({type:"End",content:content});
+
     return instructions;}
 
