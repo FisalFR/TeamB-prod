9dae6b3d539031c05d09982e507e128a5c5b84d5
khulburt <khulburt121@gmail.com>
2024-04-25 23:35:27 -0400

    changed version of webstorm

--- apps/frontend/src/routes/credits-page.tsx
+++ apps/frontend/src/routes/credits-page.tsx
@@ -19,21 +19,21 @@ export function creditsPage() {
 
                 <div className="grid grid-cols-3 place-items-center">
                 <div className="w-100">
-                        <div className=" border-2 rounded-3xl border-2 bg-gradient-to-t from-white to-blue-300 text-center">
+                        <div className=" border-2 rounded-3xl bg-gradient-to-t from-white to-blue-300 text-center">
                             <div className="h-80 my-auto">
                                 <img src={webstorm} alt="webstorm"
                                      className="object-cover w-full h-full rounded-t-3xl"/>
                             </div>
                             <div className="h-85 ">
                                 <h1 className="text-deep-blue font-bold">Webstorm</h1>
-                                <p className=" text-deep-blue font-bold">V.1.5</p>
+                                <p className=" text-deep-blue font-bold">2023.3</p>
                                 <br/>
                             </div>
                         </div>
                     </div>
 
                     <div className="w-100 ">
-                        <div className=" border-2 rounded-3xl border-2 bg-gradient-to-t from-white to-blue-300 text-center">
+                        <div className=" border-2 rounded-3xl bg-gradient-to-t from-white to-blue-300 text-center">
                             <div className="h-80 my-auto">
                                 <img src={Docker} alt="Docker"
                                      className="object-cover w-full h-full rounded-t-3xl"/>
@@ -47,7 +47,7 @@ export function creditsPage() {
                     </div>
 
                     <div className="w-100 py-10">
-                        <div className=" border-2 rounded-3xl border-2 bg-gradient-to-t from-white to-blue-300 text-center">
+                        <div className=" border-2 rounded-3xl bg-gradient-to-t from-white to-blue-300 text-center">
                             <div className="h-80 my-auto">
                                 <img src={webstorm} alt="hospital"
                                      className="object-cover w-full h-full  rounded-t-3xl"/>
@@ -61,7 +61,20 @@ export function creditsPage() {
                     </div>
 
                     <div className="w-100">
-                        <div className=" border-2 rounded-3xl border-2 bg-gradient-to-t from-white to-blue-300 text-center">
+                        <div className=" border-2 rounded-3xl bg-gradient-to-t from-white to-blue-300 text-center">
+                            <div className="h-80 my-auto">
+                                <img src={webstorm} alt="hospital"
+                                     className="object-cover w-full h-full rounded-t-3xl"/>
+                            </div>
+                            <div className="h-85 ">
+                                <h1 className="text-deep-blue font-bold">Webstorm</h1>
+                                <p className=" text-deep-blue font-bold">V</p>
+                                <br/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-100">
+                        <div className=" border-2 rounded-3xl bg-gradient-to-t from-white to-blue-300 text-center">
                             <div className="h-80 my-auto">
                                 <img src={webstorm} alt="hospital"
                                      className="object-cover w-full h-full rounded-t-3xl"/>
@@ -74,20 +87,7 @@ export function creditsPage() {
                         </div>
                     </div>
                     <div className="w-100">
-                        <div className=" border-2 rounded-3xl border-2 bg-gradient-to-t from-white to-blue-300 text-center">
-                            <div className="h-80 my-auto">
-                                <img src={webstorm} alt="hospital"
-                                     className="object-cover w-full h-full rounded-t-3xl"/>
-                            </div>
-                            <div className="h-85 ">
-                                <h1 className="text-deep-blue font-bold">Webstorm</h1>
-                                <p className=" text-deep-blue font-bold">V.1.5</p>
-                                <br/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-100">
-                        <div className=" border-2 rounded-3xl border-2 bg-gradient-to-t from-white to-blue-300 text-center">
+                        <div className=" border-2 rounded-3xl bg-gradient-to-t from-white to-blue-300 text-center">
                             <div className="h-80 my-auto">
                                 <img src={webstorm} alt="hospital"
                                      className="object-cover w-full h-full rounded-t-3xl"/>
