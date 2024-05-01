a99ceecddb0db9dcc0b2215296d88f1ae0b5ea3a
HenryHribar <hhribar21@gmail.com>
2024-04-30 21:51:12 -0400

    Fixed warning.

--- apps/frontend/src/components/ServiceCard.tsx
+++ apps/frontend/src/components/ServiceCard.tsx
@@ -6,7 +6,7 @@ function ServiceCard(props:{onClick:MouseEventHandler, icon: string, Request:str
         <div className="w-1/4 h-1/2 centerContent">
             <Button onClick={props.onClick} color={"bg-bone-white"} rounded={"rounded-2xl"}>
                 <div className="flex flex-col centerContent">
-                    <img src={props.icon} className={"size-24"}></img>
+                    <img alt="ServiceCardImage" src={props.icon} className={"size-24"}></img>
                     <p className="text-2xl font-OpenSans font-bold text-black py-4 ">{props.Request}</p>
                     <p className="font-OpenSans text-black">{props.description}</p>
                 </div>
